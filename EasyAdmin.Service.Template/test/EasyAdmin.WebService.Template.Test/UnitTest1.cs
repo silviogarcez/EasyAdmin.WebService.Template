10f8c30ba35ab87488072723cbfe4e591cdc20f5
Silvio Garcez <silvio__jr@hotmail.com>
2025-02-08 11:51:21 +0000

    getting start

---
+++ EasyAdmin.Service.Template/test/EasyAdmin.WebService.Template.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EasyAdmin.WebService.Template.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
